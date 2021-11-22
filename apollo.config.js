4b13b402619e80fa01907f2a9d299f060ff0a0b1
George Oluoko <otienog1@gmail.com>
2021-11-22 12:02:15 +0300

    apollo client config setup

--- apollo.config.js
+++ apollo.config.js
@@ -2,7 +2,7 @@ module.exports = {
     client: {
         service: {
             name: 'wpgraphql',
-            url: 'http://http://167.71.141.164/graphql',
+            url: 'http://167.71.141.164/graphql',
         }
     }
 };
