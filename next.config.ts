cdfd9c013126d6fb90ba7e6607593101823a2808
George Oluoko <otienog1@gmail.com>
2026-06-15 15:13:47 +0300

    feat: allow remote WP images for next/image

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   output: 'export',
   images: {
-    remotePatterns: [{ protocol: 'https', hostname: 'api.theluigifootprints.org' }],
+    remotePatterns: [{ protocol: 'https', hostname: 'api.theluigifootprints.org', pathname: '/wp-content/**' }],
     unoptimized: true,
   },
 }
